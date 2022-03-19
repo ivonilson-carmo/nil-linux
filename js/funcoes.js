de9567fa4029dc8b790f57aa17d49440c758d39c
ivonilson-carmo <ivonilsoncarmo3@gmail.com>
2022-03-18 23:30:04 -0300

    site responsivity implemented

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,5 +1,7 @@
 const contentsJornal = Array.from(document.querySelector('#jornal > .contents').children)
 let inicio = 1
+let wid = document.querySelector('body').clientWidth
+let quantView
 
 const changeElementsJornal = (opcao = '') => {
     if (opcao == 'next'){
@@ -8,30 +10,47 @@ const changeElementsJornal = (opcao = '') => {
         inicio--
     }
 
+    changeViewNews()
+
     for (let pos = 1; pos < inicio ; pos++ ){
         contentsJornal[pos].style.display = 'none'
         // console.log(pos, 'some')
     }
-    for (let pos = inicio ; pos < (inicio + 3); pos++ ){
+    for (let pos = inicio ; pos < (inicio + quantView); pos++ ){
         contentsJornal[pos].style.display = 'block'
         // console.log(pos, 'view')
     }
 
-    for (let pos = (inicio + 3) ; pos < (contentsJornal.length -1) ; pos++){
+    for (let pos = (inicio + quantView) ; pos < (contentsJornal.length -1) ; pos++){
         contentsJornal[pos].style.display = 'none'
         // console.log(pos, 'some')
     }
 }
 
-function volta(ele){
+function changeViewNews(){
+    if (wid > 900){
+        quantView = 3
+    }else{
+        quantView = 1
+    }
+}
+
+function voltaNoticia(ele){
     if (inicio != 1){
         changeElementsJornal(opcao='back')
     }
 }
 
-function avanca(ele){
-    if (inicio < (contentsJornal.length - 4)){
+function avancaNoticia(ele){
+    if (inicio < (contentsJornal.length - (quantView + 1))){
         changeElementsJornal(opcao='next')
+    }
+}
+
+function resizePage(){
+    changeViewNews()
+    if (wid < 602){
+        document.querySelector('.logo-header img').src = './assets/img/logo.png'
     }
 }
 
